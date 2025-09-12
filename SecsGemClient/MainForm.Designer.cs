61c53d91b5fb525d68535e178c07470a008efe06
David <david@dcsswebdev.com>
2025-09-12 11:43:23 -0700

    renamed directories to match new names

--- SecsGemClient/MainForm.Designer.cs
+++ SecsGemClient/MainForm.Designer.cs
@@ -30,7 +30,8 @@
         {
             this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            //this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Size = new Size(920, 800);
             this.Text = "SECS/GEM Client";
         }
 
